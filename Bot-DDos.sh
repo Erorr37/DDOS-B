2d3ff5c73a969e9731ca1bcc05f470a8671200cb
Erorr37 <147059073+Erorr37@users.noreply.github.com>
2023-10-21 11:35:13 +0700

    Add files via upload

---
+++ Bot-DDos.sh
@@ -0,0 +1,47 @@
+#/bin/bash
+
+clear
+
+red='\033[0;31m'
+hijau='\033[0;32m'
+kuning='\033[0;33m'
+biru='\033[0;34m'
+ungu='\033[0;35m'
+cyan='\033[0;36m'
+putih='\033[0;37m'
+brown='\e[0;33m'
+mera='\033[41m'
+hija='\033[42m'
+kun='\033[43m'
+RESET='\033[0m'
+
+
+sleep 1
+
+echo -e "${putih}░░░░░░░░░░${cyan}▄▄▄▄▄▄▄${putih}░░░░░░░░░░"
+echo -e "${putih}░░░░░░${cyan}▄▄▀▀░░░░░░${cyan}░▀▀▄▄${putih}░░░░░░"
+echo -e "${putih}░░░${cyan}░▄▀░░░░░░░░░░░░░░░▀▄${putih}░░░░    ${ungu}Welcome To Bot-DDOS"
+echo -e "${putih}░░░${cyan}▄▀░░░▄▄▄▄▄▄▄▄▄▄▄░░░░█${putih}░░░    ${ungu}Erorr37 & XNP37"
+echo -e "${putih}░░${cyan}█░░▄███████████████▄░░█${putih}░░    ${ungu}Version 1.0"
+echo -e "${putih}░${cyan}█░░▄██▀░▄▄▀███▀▄▄░▀███░░█${putih}░    ${ungu}From : TeamXIndonesia"
+echo -e "${putih}░${cyan}█░░▀█████████████████▀░░█${putih}░"
+echo -e "${putih}░${cyan}█░░░░▀▀████████████▀░░░░█${putih}░"
+echo -e "${putih}░${cyan}░█░░░░░░░░▀▀▀▀▀░░░░░░░▄▀${putih}░░"
+echo -e "${putih}░░░${cyan}▀▀▄▄▄▄░░░░░░░░░▄▄▄▀▀${putih}░░░░"
+echo ""
+echo -e "${kuning}cara menggunakan nya↓"
+echo -e "${biru}[URL] [PORT] [TIME]"
+echo ""
+echo -e "${biru}»»»»»»»»»»»»»»»»»»»»»»»»"
+echo -e "${cyan}  » HTTPS    » BOM-VIP"
+echo ""
+echo -e "  » ${cyan}HTTP     » BYERR "
+echo -e "${biru}»»»»»»»»»»»»»»»»»»»»»»»»"
+echo -e "${brown} "
+read -p "Bot-DDOS @ Vip :#> " op
+sleep 10
+echo -e "${biru}»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»"
+echo -e "${ungu}Website Error. will probably recover within 5 seconds!!"
+echo -e "${biru}»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»"
+sleep 5
+
